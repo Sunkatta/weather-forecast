3fda2a5d230f41f1ad090c6e6f77f0a47a2212d0
Lusian Manolov <lusianmanolov@outlook.com>
2020-06-17 18:43:22 +0300

    added Constants

---
+++ app/src/main/java/com/example/weatherforecast/Common/Constants.java
@@ -0,0 +1,23 @@
+package com.example.weatherforecast.Common;
+
+public class Constants {
+    private static final String PACKAGE_NAME = "com.example.weatherforecast";
+    public static final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY";
+    public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_DATA_EXTRA";
+    public static final int SUCCESS_RESULT = 1;
+    public static final int FAILURE_RESULT = 0;
+
+    public static final String FORECAST_DATE_TIME = "dt";
+    public static final String MAIN_FORECAST_INFORMATION = "main";
+    public static final String TEMPERATURE = "temp";
+    public static final String WEATHER = "weather";
+    public static final String DETAILED_WEATHER_STATUS = "description";
+    public static final String GENERAL_WEATHER_STATUS = "main";
+    public static final String ICON = "icon";
+    public static final String FEELS_LIKE = "feels_like";
+    public static final String MIN_TEMPERATURE = "temp_min";
+    public static final String MAX_TEMPERATURE = "temp_max";
+
+    public static final int NUMBER_OF_FORECASTS_IN_24_HOURS = 8;
+}
